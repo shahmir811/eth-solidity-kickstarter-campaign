dafafaf7eb8c8a8ff99d09beb73945e0bb0ef05f
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2023-07-18 12:22:21 +0500

    Added NEXT js and confgure web3 on the frontend

---
+++ ethereum/factory.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+import CampaignFactory from './build/CampaignFactory.json';
+import web3 from './web3';
+
+const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface, process.env.SEPOLIA_DEPLOYED_ADDRESS));
+
+export default instance;
