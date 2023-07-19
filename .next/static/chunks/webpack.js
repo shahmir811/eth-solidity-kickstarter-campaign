506229e79eb8b23d51762e9113f166c8b271f754
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2023-07-19 17:08:34 +0500

    Updated web3 file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45ed0743a5908430"; }
+/******/ 		__webpack_require__.h = function() { return "ef00d059891e4df7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import factory from '../ethereum/factory';
 
-const Index = () => {
+const Index = props => {
+	console.log(props);
+
 	return (
 		<div>
 			<h1>Welcome to CrowdCoin!</h1>
+			{/* <p>{props.campaigns[0]}</p> */}
 		</div>
 	);
+};
+
+Index.getInitialProps = async ({ req }) => {
+	const campaigns = await factory.methods.getDeployedCampaigns().call();
+	return {
+		campaigns,
+	};
 };
 
 export default Index;
